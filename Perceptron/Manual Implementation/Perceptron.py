e4d47e07032dc4ad38f779ea62ce047018186e9c
stanielz <stanielz@gmail.com>
2020-02-10 13:42:09 -0600

    removed use of numpy

--- Perceptron/Manual Implementation/Perceptron.py
+++ Perceptron/Manual Implementation/Perceptron.py
@@ -11,6 +11,13 @@ def col_vector(array1, array2):
 
     return col_vector_list
 
+def dot(array1, array2):
+    d_product = []
+    for i in range(0, len(array1)):
+        d_product.append(array1[i][0] * array2[0] + array1[i][1] * array2[1])
+
+    return d_product
+
 
 #  import data and name columns
 data = pd.read_csv("iris.data", sep=',')
@@ -37,19 +44,21 @@ data['Result'] = result_list
 sepal = np.array(data['sepal width in cm'])
 petal = np.array(data['petal width in cm'])
 features = list(zip(sepal, petal))[0:100]
-w = np.array([0, 0])
-x = np.dot(features, w)
+w = [0, 0]
+#x = np.dot(features, w)
+x = dot(features, w)
+#print(x)
 results = []
 count = 1
 
 #  tests and updates weight(w) until we have more than 50 consecutive predictions on learning data
 #  s variable guarantees even index for learning
-while count <= 49:
-    x = np.dot(features, w)
+while count <= 50:
+    x = dot(features, w)
     s = 2 * random.randint(0, 49)
     for s in range(0, 49):
         if (int(x[2 * s]) >= 0 and int(result_list[2 * s]) < 0) or (int(x[2 * s]) < 0 and int(result_list[2 * s]) >= 0):
-            w = w + result_list[2 * s] * np.transpose(features[2 * s])
+            w = [w[0] + result_list[2 * s] * features[2 * s][0], w[1] + result_list[2 * s] * features[2 * s][1]]
             count = 0
         else:
             count += 1
@@ -66,6 +75,7 @@ for i in range(0, 49):
     x2_validate.append(result_list[2 * i + 1])
 
 #  plots predicted data vs actual results
+print(w)
 plt.plot(x2_validate, 'o', color='red', label='actual results')
 plt.plot(x_validate, 'o', color='blue', label='predicted results')
 plt.legend(loc='center')
